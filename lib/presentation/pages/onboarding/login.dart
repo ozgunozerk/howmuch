58fd0b2bc8de9a6f2f35987f2ebbf6825d5bca4e
Özgün Özerk <ozgun@subspace.network>
2023-12-15 15:28:49 +0300

    howmuch logo text for login screen

--- lib/presentation/pages/onboarding/login.dart
+++ lib/presentation/pages/onboarding/login.dart
@@ -14,57 +14,64 @@ class LoginPage extends GetView<LoginController> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text("Login / SignUp"),
-      ),
       body: SafeArea(
-        child: Center(
-          child: Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 32),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                ElevatedButton.icon(
-                  onPressed: () async {
-                    bool loginSuccessful = await controller.googleLogin();
-                    if (loginSuccessful) {
-                      // Push a new page with only loading animation
-                      loadingAnimation();
-                      (await SharedPreferences.getInstance())
-                          .setBool('isLoggedIn', true); // Save the login status
-                      await controller
-                          .afterLogin(); // `afterLogin` will transition into a new page after it's completed
-                    }
-                  },
-                  style: ElevatedButton.styleFrom(
-                      foregroundColor: howBlack,
-                      backgroundColor: howWhite,
-                      minimumSize: const Size(double.infinity, 50)),
-                  icon: const Icon(Ionicons.logo_google),
-                  label: const Text("Sign up with Google"),
-                ),
-                const Padding(padding: EdgeInsets.all(16)),
-                ElevatedButton.icon(
-                  onPressed: () async {
-                    bool loginSuccessful = await controller.appleLogin();
-                    if (loginSuccessful) {
-                      // Push a new page with only loading animation
-                      loadingAnimation();
-                      (await SharedPreferences.getInstance())
-                          .setBool('isLoggedIn', true); // Save the login status
-                      await controller
-                          .afterLogin(); // `afterLogin` will transition into a new page after it's completed
-                    }
-                  },
-                  style: ElevatedButton.styleFrom(
-                      foregroundColor: howBlack,
-                      backgroundColor: howWhite,
-                      minimumSize: const Size(double.infinity, 50)),
-                  icon: const Icon(Ionicons.logo_apple),
-                  label: const Text("Sign up with Apple"),
-                ),
-              ],
-            ),
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 32),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+              const Padding(padding: EdgeInsets.all(48)),
+              const SizedBox(
+                  width: 350,
+                  height: 350,
+                  child:
+                      Image(image: AssetImage('assets/HM_LOGO-TYPE_02.png'))),
+              const Spacer(),
+              Column(
+                children: [
+                  ElevatedButton.icon(
+                    onPressed: () async {
+                      bool loginSuccessful = await controller.googleLogin();
+                      if (loginSuccessful) {
+                        // Push a new page with only loading animation
+                        loadingAnimation();
+                        (await SharedPreferences.getInstance()).setBool(
+                            'isLoggedIn', true); // Save the login status
+                        await controller
+                            .afterLogin(); // `afterLogin` will transition into a new page after it's completed
+                      }
+                    },
+                    style: ElevatedButton.styleFrom(
+                        foregroundColor: howBlack,
+                        backgroundColor: howWhite,
+                        minimumSize: const Size(double.infinity, 50)),
+                    icon: const Icon(Ionicons.logo_google),
+                    label: const Text("Sign up with Google"),
+                  ),
+                  const Padding(padding: EdgeInsets.all(16)),
+                  ElevatedButton.icon(
+                    onPressed: () async {
+                      bool loginSuccessful = await controller.appleLogin();
+                      if (loginSuccessful) {
+                        // Push a new page with only loading animation
+                        loadingAnimation();
+                        (await SharedPreferences.getInstance()).setBool(
+                            'isLoggedIn', true); // Save the login status
+                        await controller
+                            .afterLogin(); // `afterLogin` will transition into a new page after it's completed
+                      }
+                    },
+                    style: ElevatedButton.styleFrom(
+                        foregroundColor: howBlack,
+                        backgroundColor: howWhite,
+                        minimumSize: const Size(double.infinity, 50)),
+                    icon: const Icon(Ionicons.logo_apple),
+                    label: const Text("Sign up with Apple"),
+                  ),
+                  const Padding(padding: EdgeInsets.all(48)),
+                ],
+              ),
+            ],
           ),
         ),
       ),
